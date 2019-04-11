5be855469d6a99c2045be98e085baabad8d694db
Samkit Nareshbhai Shah <745113@student.tor.loc>
2019-04-11 11:32:32 -0400

    Commit Third for lab 2

--- mapscript.js
+++ mapscript.js
@@ -1,19 +1,21 @@
-var map1, map2, map3;
+var map1, map2, map3 ;
+
 function initialize(){
-    var mapOption1 = {
+    var mapOptions1 = {
         zoom:2,
-        center: {lat:40.758904, lng:-73.985128}
+        center:{lat: 40.758904, lng:-73.985128}
     };
-    var mapOption2 = {
+    var mapOptions2 = {
         zoom:10,
-        center: {lat:40.758904, lng:-73.985128}
+        center:{lat:40.758904,lng:-73.985128}
     };
-    var mapOption3 = {
+    var mapOptions3={
         zoom:18,
-        center: {lat:40.758904, lng:-73.985128}
+        center:{lat:40.758904, lng:-73.985128}
     };
-    map1= new google.mapa.Map(document.getElementById('map_canvas1'),mapOptions1);
-    map1= new google.mapa.Map(document.getElementById('map_canvas2'),mapOptions2);
-    map1= new google.mapa.Map(document.getElementById('map_canvas3'),mapOptions3);
+    map1= new google.maps.Map(document.getElementById('map_canvas1'),mapOptions1);
+    map2= new google.maps.Map(document.getElementById('map_canvas2'),mapOptions2);
+    map3= new google.maps.Map(document.getElementById('map_canvas3'),mapOptions3);
+
 }
-google.maps.event.addDomlidstener(window,'load',initialize);
+google.maps.event.addDomListener(window,'load',initialize);
